4a8922998e2a9b748da8d357b56906e2a9ceac7e
Abhinav Joshi <abhinavjoshi@Abhinavs-MacBook-Pro-2.local>
2025-06-05 18:44:44 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 function App() {
   return (
     <div className="bg-red-300 p-4">
-  <p className="text-green-500 font-bold text-center">Hi Mom!</p>
+  <p className="text-green-500 font-bold text-center">
+    'Options Tracker'
+  </p>
     </div>
 
   );
